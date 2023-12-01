8f7600c148352354d0881119a29bd212305a4f6f
Salvatore <mammoliti.sam@gmail.com>
2023-12-01 10:58:55 -0500

    add script.js variables

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+var wordBlank = document.querySelector(".word-blank");
+var win = document.querySelector(".win");
+var lose = document.querySelector(".lose");
+var timerElement = document.querySelector(".timer-count");
+var startButton = document.querySelector(".start-button");
+
+var chosenWord = "";
+var numBlanks = 0;
+var winCounter = 0;
+var loseCounter = 0;
+var isWin = false;
+var timer;
+var timerCount;
